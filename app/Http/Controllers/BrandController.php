5dd17b2635b94f24a9989c4edd6c73bbf4abd26c
rabbaniST <golamrabbanig5@gmail.com>
2024-02-09 11:51:28 +0600

    Brand Data retrive from database for backend

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Brand;
+use App\Helper\ResponseHelper;
+use Illuminate\Http\JsonResponse;
 
 class BrandController extends Controller
 {
-    //
+    public function BrandList():JsonResponse
+    {
+        $data = Brand::all();
+        return ResponseHelper::Output('success', $data, 200);
+    }
 }
